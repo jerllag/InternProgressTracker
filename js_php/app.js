8106091631d68185c45b2b5d6befd3566d861200
jerllag <jeromellaguno@yahoo.com>
2015-08-23 12:44:45 +0800

    change ngRoute to ui-router
    change ngRoute to ui-router
    pinaghiwalay ung navbar para sa login at ndi

--- js_php/app.js
+++ js_php/app.js
@@ -1,6 +1,6 @@
-var app = angular.module("InProgTrack",['ngRoute','ui.bootstrap', 'ngCookies', 'angularFileUpload', 'ng-context-menu']);
+var app = angular.module("InProgTrack",['ui.router','ui.bootstrap', 'ngCookies', 'angularFileUpload', 'ng-context-menu']);
 
-app.controller("LogInController", function($http, $scope, $location, $cookieStore, $anchorScroll) {
+app.controller("LogInController", function($http, $scope, $location, $cookieStore, $state) {
 	$cookieStore.put('hasUser', false);
 	$scope.loginData = {};
 	
@@ -11,13 +11,13 @@ app.controller("LogInController", function($http, $scope, $location, $cookieStor
 		params: {studno: $scope.loginData.studno, password: $scope.loginData.password}
 		})
 		.success(function(data) {
-			/*$cookieStore.put('studno', data[0][0]);
+			$cookieStore.put('studno', data[0][0]);
 			$cookieStore.put('fname', data[0][1]);
 			$cookieStore.put('lname', data[0][2]);
 			$cookieStore.put('email', data[0][3]);
 			$cookieStore.put('password', data[0][4]);
-			$cookieStore.put('hasUser', true);*/
-			$location.path("/leaderboard");
+			$cookieStore.put('hasUser', true);
+			$state.go("navbarlogin.leaderboard");
 			
 		})
 		.error(function() {
@@ -25,14 +25,9 @@ app.controller("LogInController", function($http, $scope, $location, $cookieStor
 		})
 		;
 	};
-	
-	$scope.scrollTo = function(id) {
-      $location.hash(id);
-      $anchorScroll();
-	};
 });
 
-app.controller("navbarController", function($location, $cookieStore, $scope) {
+app.controller("navbarController", function($location, $cookieStore, $scope, $anchorScroll, $state) {
 	$scope.signOut = function() {
 		$cookieStore.remove('studno');
 		$cookieStore.remove('fname');
@@ -40,7 +35,12 @@ app.controller("navbarController", function($location, $cookieStore, $scope) {
 		$cookieStore.remove('email');
 		$cookieStore.remove('password');
 		$cookieStore.put('hasUser', false);
-		$location.path("/");
+		$state.go("navbar.login");
+	};
+	
+	$scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
 	};
 });
 
@@ -51,7 +51,7 @@ app.controller("SignUpController", function($http, $scope, $log, $location, $coo
 		
 		$http.post('js_php/studentCRU.php', {studno: $scope.info.studno, fname: $scope.info.fname, lname: $scope.info.lname, course: $scope.info.course,
 											 college: $scope.info.college, contactno: $scope.info.contactno, email: $scope.info.email, password: $scope.info.password,
-											 compname: $scope.info.compname, startdate: 2015-01-01, enddate: 2015-01-02, hoursrequired: $scope.info.hoursrequired,
+											 compname: $scope.info.compname, startdate: "2015-01-01", enddate: "2015-01-02", hoursrequired: $scope.info.hoursrequired,
 											 compadd: $scope.info.compadd, deptass: $scope.info.deptass, conperson: $scope.info.conperson,
 											 cpcontactno: $scope.info.cpcontactno, cpemail: $scope.info.cpemail})
 			.success(function(data) {
@@ -61,31 +61,44 @@ app.controller("SignUpController", function($http, $scope, $log, $location, $coo
 				alert(data);
 			});
 	};
-
+	
 	$scope.scrollTo = function(id) {
       $location.hash(id);
       $anchorScroll();
 	};
 });
 
-app.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'html/login.html'
-    })
-    .when('/signup', {
-      templateUrl: 'html/signup.html'
-    })
-	.when('/leaderboard', {
-      templateUrl: 'html/leaderboard.html'
-    })
-	.when('/profile', {
-      templateUrl: 'html/profile.html'
-    })
-	.when('/accomplishmentReport', {
-      templateUrl: 'html/accomReport.html'
-    })
-	.otherwise ({
-		redirectTo: '/'
-	});
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/login");
+
+	$stateProvider
+		.state('navbar', {
+			abstract: true,
+			url: "",
+			templateUrl: "html/navbar.html"
+		})
+		.state('navbar.login', {
+			url: "/login",
+			templateUrl: "html/login.html"
+		})
+		.state('navbar.signup', {
+			url: "/signup",
+			templateUrl: "html/signup.html"
+		})
+		.state('navbarlogin', {
+			url: "/logined",
+			templateUrl: "html/navbarlogin.html"
+		})
+		.state('navbarlogin.leaderboard', {
+			url: "/leaderboard",
+			templateUrl: "html/leaderboard.html"
+		})
+		.state('navbarlogin.profile', {
+			url: "/profile",
+			templateUrl: "html/profile.html"
+		})
+		.state('navbarlogin.accomReport', {
+			url: "/accomReport",
+			templateUrl: "html/accomReport.html"
+		});
 });
